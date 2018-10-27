ffb6c6adabe3727d42ff3a59845b55f40b568dcc
Lakraa Omer <omer.lakraa@hotmail.fr>
2018-10-27 20:31:52 +0200

    Creation renderer

--- main.c
+++ main.c
@@ -5,26 +5,43 @@
 int main(int argc, char *argv[])
 {
     SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+    int statut = EXIT_FAILURE;
 
     if(0 != SDL_Init(SDL_INIT_VIDEO))
     {
         fprintf(stderr, "Erreur SDL_Init : %s", SDL_GetError());
-        return EXIT_FAILURE;
+        goto Quit;
     }
 
-    // Creation de la fenetre
+    // Creation of window
     window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
                               640, 480, SDL_WINDOW_SHOWN);
     if(NULL == window)
     {
         fprintf(stderr, "Erreur SDL_CreateWindow : %s", SDL_GetError());
-        return EXIT_FAILURE;
+        goto Quit;
     }
+
+    // Creation of Rendere
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if(NULL == renderer)
+    {
+        fprintf(stderr, "Erreur SDL_CreateRenderer : %s", SDL_GetError());
+        goto Quit;
+    }
+
+    statut = EXIT_SUCCESS;
 
     // User Code...
     SDL_Delay(3000);
 
 
+Quit: // Label of quit
+    if(NULL != renderer)
+        SDL_DestroyRenderer(renderer);
+    if(NULL != window)
+        SDL_DestroyWindow(window);
     SDL_Quit();
-return EXIT_SUCCESS;
+    return statut;
 }
