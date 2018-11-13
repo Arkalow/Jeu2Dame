ac794c8122ead4cd0b42b4f74c68e0e26cc293ec
Omer Lakraa <omer.lakraa@hotmail.fr>
2018-11-13 14:11:55 +0100

    Ajout des structures menu et item_menu et ajout des fonction createMenu et createItem et de la fonction showMenu

--- interfaces/sdl/Menu.c
+++ interfaces/sdl/Menu.c
@@ -10,4 +10,46 @@
 #include "../../mods/Player.h"
 #include "sdlBoard.h"
 #include "gui.h"
+#include "Menu.h"
 
+/**
+ * Créer un menu
+ */
+struct Menu createMenu(
+    struct Item_menu * items,
+    SDL_Point positionStartItem, 
+    SDL_Color backgroundColor,
+    SDL_Color fontColor
+){
+    struct Menu menu;
+    menu.items = items;
+    menu.positionStartItem = positionStartItem;
+    menu.backgroundColor = backgroundColor;
+    menu.fontColor = fontColor;
+    menu.show = showMenu;
+    return menu;
+}
+
+/**
+ * Créer un item
+ */
+struct Item_menu createItem(
+    SDL_Rect position,
+	int selected,
+    SDL_Color backgroundColor,
+    SDL_Color fontColor,
+    void (*click)(void)
+){
+    struct Item_menu item;
+    item.position = position;
+    item.selected = selected;
+    item.backgroundColor = backgroundColor;
+    item.fontColor = fontColor;
+    item.click = click;
+    return item;
+}
+
+
+void showMenu(){
+    printf("showMenu !!!");
+}
--- interfaces/sdl/Menu.h
+++ interfaces/sdl/Menu.h
@@ -2,3 +2,56 @@
  * Gere la les menus dans la SDL
  */
 
+/**
+ *	Représente un menu
+ */
+struct Menu
+{
+    struct Item_menu * items;
+    int nbItem;
+
+    SDL_Point positionStartItem;
+
+    SDL_Color backgroundColor;
+    SDL_Color fontColor;
+
+    void (*show)(void);
+};
+
+/**
+ *	Représente un item de menu
+ */
+struct Item_menu
+{
+    SDL_Rect position;
+	int selected; // Indique si le pion est selectionné
+
+    SDL_Color backgroundColor;
+    SDL_Color fontColor;
+
+    void (*click)(void); // Fonction executé au click
+    //void (*select)(void); // Fonction executé au survol
+};
+
+/**
+ * Créer un menu
+ */
+struct Menu createMenu(
+    struct Item_menu * items,
+    SDL_Point positionStartItem, 
+    SDL_Color backgroundColor,
+    SDL_Color fontColor
+);
+
+/**
+ * Créer un item
+ */
+struct Item_menu createItem(
+    SDL_Rect position,
+	int selected,
+    SDL_Color backgroundColor,
+    SDL_Color fontColor,
+    void (*click)(void)
+);
+
+void showMenu();
